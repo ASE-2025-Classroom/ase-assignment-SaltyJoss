7b311ec2a84cde79b715e0356970f5b356ce59c3
saltyjoss <joss.salton@gmail.com>
2025-10-04 16:32:38 +0100

    Add new buttons to Form1.Design.cs (Load, Save, Reset) Add new TextBox to to Form1.Design.cs (textDebug) for debugging and text-based information without interrupting the inputted commands

--- BOOSECoreApp/UI/Form1.Designer.cs
+++ BOOSECoreApp/UI/Form1.Designer.cs
@@ -31,8 +31,12 @@
             printDialog1 = new PrintDialog();
             txtProgram = new TextBox();
             outputCanvas = new PictureBox();
-            runButton = new Button();
             clearButton = new Button();
+            runButton = new Button();
+            saveButton = new Button();
+            loadButton = new Button();
+            textDebug = new TextBox();
+            resetButton = new Button();
             ((System.ComponentModel.ISupportInitialize)outputCanvas).BeginInit();
             SuspendLayout();
             // 
@@ -45,47 +49,89 @@
             txtProgram.Location = new Point(12, 12);
             txtProgram.Multiline = true;
             txtProgram.Name = "txtProgram";
-            txtProgram.Size = new Size(426, 622);
+            txtProgram.Size = new Size(426, 1017);
             txtProgram.TabIndex = 0;
             txtProgram.TextChanged += txtProgram_TextChanged;
             // 
             // outputCanvas
             // 
-            outputCanvas.Location = new Point(453, 12);
+            outputCanvas.Location = new Point(444, 12);
             outputCanvas.Name = "outputCanvas";
-            outputCanvas.Size = new Size(1199, 657);
+            outputCanvas.Size = new Size(1448, 668);
             outputCanvas.TabIndex = 1;
             outputCanvas.TabStop = false;
             outputCanvas.Click += outputCanvas_Click;
             // 
+            // clearButton
+            // 
+            clearButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            clearButton.Location = new Point(444, 975);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(210, 54);
+            clearButton.TabIndex = 3;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
             // runButton
             // 
             runButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            runButton.Location = new Point(12, 640);
+            runButton.Location = new Point(444, 905);
             runButton.Name = "runButton";
-            runButton.Size = new Size(210, 29);
+            runButton.Size = new Size(210, 54);
             runButton.TabIndex = 2;
             runButton.Text = "Run";
             runButton.UseVisualStyleBackColor = true;
             runButton.Click += runButton_Click;
             // 
-            // clearButton
+            // saveButton
             // 
-            clearButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            clearButton.Location = new Point(228, 640);
-            clearButton.Name = "clearButton";
-            clearButton.Size = new Size(210, 29);
-            clearButton.TabIndex = 3;
-            clearButton.Text = "Clear";
-            clearButton.UseVisualStyleBackColor = true;
-            clearButton.Click += clearButton_Click;
+            saveButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            saveButton.Location = new Point(444, 695);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(210, 54);
+            saveButton.TabIndex = 4;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            loadButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            loadButton.Location = new Point(444, 765);
+            loadButton.Name = "loadButton";
+            loadButton.Size = new Size(210, 54);
+            loadButton.TabIndex = 5;
+            loadButton.Text = "Load";
+            loadButton.UseVisualStyleBackColor = true;
+            // 
+            // textDebug
+            // 
+            textDebug.Location = new Point(660, 695);
+            textDebug.Multiline = true;
+            textDebug.Name = "textDebug";
+            textDebug.Size = new Size(1232, 334);
+            textDebug.TabIndex = 6;
+            // 
+            // resetButton
+            // 
+            resetButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            resetButton.Location = new Point(444, 835);
+            resetButton.Name = "resetButton";
+            resetButton.Size = new Size(210, 54);
+            resetButton.TabIndex = 8;
+            resetButton.Text = "Reset";
+            resetButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(1664, 681);
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(resetButton);
+            Controls.Add(textDebug);
+            Controls.Add(loadButton);
+            Controls.Add(saveButton);
             Controls.Add(clearButton);
             Controls.Add(runButton);
             Controls.Add(outputCanvas);
@@ -93,6 +139,7 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Form1";
+            Load += Form1_Load_1;
             ((System.ComponentModel.ISupportInitialize)outputCanvas).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -103,7 +150,11 @@
         private PrintDialog printDialog1;
         private TextBox txtProgram;
         private PictureBox outputCanvas;
-        private Button runButton;
         private Button clearButton;
+        private Button runButton;
+        private Button saveButton;
+        private Button loadButton;
+        private TextBox textDebug;
+        private Button resetButton;
     }
 }
